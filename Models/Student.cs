b5b3de672473fef7394e5b0a54de865b726b1a33
oyjjpp <ouyangjuncust@aliyun.com>
2021-11-14 23:21:53 +0800

    student model

---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cornucopia.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
--- Models/WeatherForecast.cs
+++ Models/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace cornucopia
+namespace cornucopia.Models
 {
     public class WeatherForecast
     {
